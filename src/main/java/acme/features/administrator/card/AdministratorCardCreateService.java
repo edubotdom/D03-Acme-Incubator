de3e8f29eb174d16ea6bbf10858357c26828ffb3
edubotdom <55277082+edubotdom@users.noreply.github.com>
2020-06-11 22:16:20 +0200

    First steps of Banner and Card CRUD operation features.

---
+++ src/main/java/acme/features/administrator/card/AdministratorCardCreateService.java
@@ -0,0 +1,72 @@
+
+package acme.features.administrator.card;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.cards.Card;
+import acme.framework.components.Errors;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Administrator;
+import acme.framework.services.AbstractCreateService;
+
+@Service
+public class AdministratorCardCreateService implements AbstractCreateService<Administrator, Card> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	AdministratorCardRepository repository;
+
+
+	@Override
+	public boolean authorise(final Request<Card> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public void bind(final Request<Card> request, final Card entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		request.bind(entity, errors);
+
+	}
+
+	@Override
+	public void unbind(final Request<Card> request, final Card entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "holder", "number", "brand", "cvv");
+	}
+
+	@Override
+	public Card instantiate(final Request<Card> request) {
+		Card result;
+		result = new Card();
+
+		return result;
+	}
+
+	@Override
+	public void validate(final Request<Card> request, final Card entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+	}
+
+	@Override
+	public void create(final Request<Card> request, final Card entity) {
+
+		this.repository.save(entity);
+
+	}
+
+}
